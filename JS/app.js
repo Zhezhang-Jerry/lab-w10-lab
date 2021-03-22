fd9ec0446c109d9604d81e7c082921349140cb02
Jerry <zzhang155@my.bcit.ca>
2021-03-22 13:52:14 -0700

    finish the app.js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,23 @@
-// your JavaScript goes here, you can delete this comment
+const todoList = {
+    todo1: "clone repo for starter code", status1: "complete",
+    todo2: "add css and js to index", status2: "complete",
+    todo3: "finish the app.js", status3: "started",
+    todo4: "finish the lab", status4: "started"
+}
+const todoArr = []
+const completedTodos = {}
+
+for (const key in todoList) {
+    todoArr.push(key, todoList[key])
+}
+
+for (const i in todoArr) {
+    if (todoArr[i] === "complete") {
+        completedTodos[todoArr[i-3]] = todoArr[i-2]
+    }
+
+}
+console.log(completedTodos)
+for (const key in completedTodos) {
+    console.log(completedTodos[key])
+}
